7491a62afc0fc437c5c3be8132ab87e0a1a177fd
alexiskov <alexiskov.rus@gmail.com>
2022-08-16 16:43:00 +0300

    настроить частичную выгрузку через xhr

--- fs.go
+++ fs.go
@@ -1,42 +1,51 @@
 package main
 
 import (
+	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 	"time"
-	"html/template"
 )
 
-type(
-	TemplateData struct{
-	  Title string
+type (
+	// Структура для хранения данных о загруженном файле
+	dirFiles struct {
+		Name, Extension, Adress string
+		Size                    int64
+		IsDir                   bool
+	}
+	TemplateData struct {
+		Files []dirFiles
+		Title string
 	}
 	Router struct{}
 )
 
-// Структура для хранения данных о загруженном файле
-type dirFiles struct {
-	Name, Extension string
-	Size            int64
-	IsDir           bool
-}
-
 const (
-	ip         string = "192.168.101.87"
-	uploadPath        = "upload"
+	ip             string = "192.168.101.87"
+	uploadPath     string = "upload"
+	fileServerPort string = ":5460"
 )
 
-var(
-	templ, _ = template.ParseFiles("./templates/template.html")
-	srv = &http.Server{
+var (
+	templatesPaths = []string{
+		"./templates/template.html",
+		"./templates/tempData.html",
+	}
+	srv      = &http.Server{
 		Addr:         ":8080",
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 5 * time.Second,
 	}
 )
+
+//Фильтр содержимого файлсервера
+func filter(fileArr []dirFiles, pattern string){
+
+}
 
 // Функция находит файлы и папки в директории; возвращает: []dirFiles{}, error
 func listFtpFiles(directory string) ([]dirFiles, error) {
@@ -47,14 +56,14 @@ func listFtpFiles(directory string) ([]dirFiles, error) {
 	thisDirFiles := make([]dirFiles, len(files))
 	for i, file := range files {
 		if !file.IsDir() {
-			entity, err := checkFileInfo(directory + "/" +file.Name())
+			entity, err := checkFileInfo(directory + "/" + file.Name())
 			if err != nil {
 				return []dirFiles{}, err
 			} else {
 				thisDirFiles[i] = entity
 			}
 		} else {
-			thisDirFiles[i] = dirFiles{file.Name(), "Папка", 0, true}
+			thisDirFiles[i] = dirFiles{file.Name(), "Папка", "", 0, true}
 		}
 	}
 	return thisDirFiles, nil
@@ -71,7 +80,7 @@ func checkFileInfo(fileName string) (dirFiles, error) {
 	if err != nil {
 		return dirFiles{}, err
 	}
-	fileEntity := dirFiles{fi.Name(), filepath.Ext(fi.Name()), fi.Size(), false}
+	fileEntity := dirFiles{fi.Name(), filepath.Ext(fi.Name()), "http://" + ip + fileServerPort + "/" + fi.Name(), fi.Size(), false}
 	return fileEntity, nil
 }
 
@@ -79,21 +88,34 @@ func checkFileInfo(fileName string) (dirFiles, error) {
 func (e Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
+			templ, err := template.ParseFiles(templatesPaths...)
+			if err!=nil{
+				log.Println(err)
+				return
+			}
+
 		fileStack, err := listFtpFiles(uploadPath)
 		if err != nil {
 			log.Println(err)
 			return
 		}
-		td:=TemplateData{Title: "Главная",}
-		templ.Execute(w, td)
-		log.Println(fileStack)
-	case "/upload":
-		td:=TemplateData{Title: "Главная",}
-		templ.Execute(w, td)
+		td := TemplateData{fileStack, "Облако"}
+		if r.Method==http.MethodGet && r.FormValue("filter")!=""{
+			filterKey:=r.FormValue("filter")
+			filterVal:=r.FormValue("pattrn")
+			log.Println(filterKey, filterVal)
+			templ.ExecuteTemplate(w, "fileData",td)
+		} else{
+			err=templ.ExecuteTemplate(w, "main",td)
+			if err!=nil{
+				log.Println(err)
+				return
+			}
+		}
 	case "/exchange":
-		w.Header().Add("Access-Control-Allow-Headers","Content-Type")
-    w.Header().Add("Access-Control-Allow-Origin","*")
-	  w.Header().Set("Content-type","multipart/form-data")
+		w.Header().Add("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Content-type", "multipart/form-data")
 		file, header, err := r.FormFile("fileUpload")
 		if err != nil {
 			w.Write([]byte("Ошибка загрузки файла"))
@@ -120,7 +142,13 @@ func (e Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.Handle("/", Router{})
+	go srv.ListenAndServe()
 
-	srv.ListenAndServe()
-
+	fs := &http.Server{
+		Addr:         fileServerPort,
+		Handler:      http.FileServer(http.Dir(uploadPath)),
+		WriteTimeout: 10 * time.Second,
+		ReadTimeout:  10 * time.Second,
+	}
+	fs.ListenAndServe()
 }
